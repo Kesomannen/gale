2ece32a879b79d7c1fd40481348838f7cb29a7e1
Kesomannen <bobbo.nidor@gmail.com>
2024-07-02 12:02:53 +0200

    remove enchanced img

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		enhancedImages(),
 		sveltekit()
 	]
 });
