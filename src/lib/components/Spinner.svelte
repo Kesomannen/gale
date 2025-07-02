72a3306869bf099353e89a68b61250771e37c696
Kesomannen <bobbo.nidor@gmail.com>
2025-07-02 12:29:10 +0200

    Fix icon class

--- src/lib/components/Spinner.svelte
+++ src/lib/components/Spinner.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import clsx from 'clsx';
 	import type { ClassValue } from 'svelte/elements';
 
 	type Props = {
 		class?: ClassValue;
 	};
 
-	let { class: classProp = '' }: Props = $props();
+	let { class: classProp }: Props = $props();
 </script>
 
-<Icon icon="mdi:loading" class="{classProp} animate-spin" />
+<Icon icon="mdi:loading" class={clsx([classProp, 'animate-spin'])} />
