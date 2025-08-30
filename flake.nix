eac73a792da0f554f0a8a8c0fa4869c16b4b7e7b
Louis Orleans <louis@orleans.io>
2025-08-30 10:52:04 -0700

    📌 create nix flake for system dep management

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.alejandra);
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_20
+            corepack
+            pnpm
+            # For systems that do not ship with Python by default (required by `node-gyp`)
+            python3
+            rustc
+            cargo
+            cargo-tauri
+          ];
+        };
+      });
+    };
+}
