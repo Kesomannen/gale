07056a5db723cc6415de9870f126e4de6048db67
Bilibili_MengChu <2133404320@qq.com>
2025-09-14 19:30:11 +0800

    Add `PathPref.svelte` to translations

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,5 +1,6 @@
 import { m } from '$lib/paraglide/messages';
 import { setLocale, type Locale } from '$lib/paraglide/runtime';
+import { toSentenceCase as ToSentenceCase  } from 'js-convert-case';
 
 export function changeLanguage(lang: Locale | string) {
     setLocale(lang as Locale);
@@ -9,6 +10,18 @@ export function changeLanguage(lang: Locale | string) {
 export const languageTitle: Record<Locale, () => string> = {
     "en": m.language_en,
     "zh-CN": m.language_zh_CN
+}
+
+export function isEnglish(str: string): boolean {
+    return /^[a-zA-Z]+$/.test(str);
+}
+
+export function toSentenceCase(str: string): string {
+    if (isEnglish(str)) {
+        return ToSentenceCase(str);
+    } else {
+        return str; // Return the original string if it's not English
+    }
 }
 
 export const locales = Object.keys(languageTitle) as Locale[];
