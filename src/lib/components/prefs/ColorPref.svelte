da32ca5a09adcdec5dde719f2d52bbac6ad6c74c
Bilibili_MengChu <2133404320@qq.com>
2025-09-14 17:37:14 +0800

    add `ColorPref.svelte`

--- src/lib/components/prefs/ColorPref.svelte
+++ src/lib/components/prefs/ColorPref.svelte
@@ -16,6 +16,7 @@
 	import Info from '../ui/Info.svelte';
 	import type { Snippet } from 'svelte';
 	import clsx from 'clsx';
+	import { m } from '$lib/paraglide/messages';
 
 	type Props = {
 		category: ColorCategory;
@@ -33,10 +34,36 @@
 		value = color;
 		setColor(category, color);
 	}
+
+	const colorLabels: Record<string, () => string> = {
+		custom: m.colorPref_color_custom,
+		slate: m.colorPref_color_slate,
+		gray: m.colorPref_color_gray,
+		zinc: m.colorPref_color_zinc,
+		neutral: m.colorPref_color_neutral,
+		stone: m.colorPref_color_stone,
+		red: m.colorPref_color_red,
+		orange: m.colorPref_color_orange,
+		amber: m.colorPref_color_amber,
+		yellow: m.colorPref_color_yellow,
+		lime: m.colorPref_color_lime,
+		green: m.colorPref_color_green,
+		emerald: m.colorPref_color_emerald,
+		teal: m.colorPref_color_teal,
+		cyan: m.colorPref_color_cyan,
+		sky: m.colorPref_color_sky,
+		blue: m.colorPref_color_blue,
+		indigo: m.colorPref_color_indigo,
+		violet: m.colorPref_color_violet,
+		purple: m.colorPref_color_purple,
+		fuchsia: m.colorPref_color_fuchsia,
+		pink: m.colorPref_color_pink,
+		rose: m.colorPref_color_rose
+	};
 </script>
 
 <div class="flex items-center">
-	<Label>{capitalize(category)} color</Label>
+	<Label>{m[`colorPref_title_${category}`]()}</Label>
 
 	<Info>
 		{@render children()}
@@ -54,7 +81,9 @@
 						: { type: 'default', name: selectValue }
 				)
 		}
-		items={selectItems(selectOptions, capitalize)}
+		items={selectItems(selectOptions, item => {
+			return colorLabels[item] ? colorLabels[item]() : capitalize(item);
+		})}
 	>
 		{#snippet label({ defaultLabel })}
 			{@render colorIcon(value)}
