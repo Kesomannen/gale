0ca592b68226555245f5f272a0c338ec472fd8ee
Kesomannen <bobbo.nidor@gmail.com>
2025-10-14 16:13:31 +0200

    Tweak steam launch option ui
    Makes it look a bit more inline with the rest of the ui

--- src/lib/components/dialogs/LaunchOptionsDialog.svelte
+++ src/lib/components/dialogs/LaunchOptionsDialog.svelte
@@ -4,6 +4,7 @@
 	import { RadioGroup } from 'bits-ui';
 	import type { LaunchOption } from '$lib/types';
 	import { formatLaunchOptionName } from '$lib/util';
+	import Icon from '@iconify/svelte';
 
 	interface Props {
 		open: boolean;
@@ -35,33 +36,32 @@
 	<p class="text-primary-400 mb-4">Select how you want to launch the game:</p>
 
 	<div class="max-h-80 overflow-y-auto">
-		<RadioGroup.Root bind:value={selectedOption} class="flex flex-col space-y-2">
+		<RadioGroup.Root bind:value={selectedOption} class="flex flex-col gap-1">
 			{#each options as option}
-				<RadioGroup.Item value={option.arguments} class="group">
-					<div
-						class={[
-							'flex cursor-pointer items-center rounded-lg border p-3 transition-colors',
-							selectedOption === option.arguments
-								? 'border-primary-500 bg-primary-700'
-								: 'hover:bg-primary-800 border-transparent'
-						]}
-					>
-						<div class="mr-3 flex h-5 w-5 items-center justify-center">
-							<div
-								class={[
-									'flex h-4 w-4 items-center justify-center rounded-full border-2 transition-colors',
-									selectedOption === option.arguments ? 'border-accent-400' : 'border-primary-400'
-								]}
-							>
-								{#if selectedOption === option.arguments}
-									<div class="bg-accent-400 h-2 w-2 rounded-full"></div>
-								{/if}
-							</div>
+				<RadioGroup.Item
+					value={option.arguments}
+					class={[
+						'flex cursor-pointer items-center rounded-lg border p-3',
+						selectedOption === option.arguments
+							? 'border-primary-500 bg-primary-700'
+							: 'hover:bg-primary-700 border-transparent'
+					]}
+				>
+					<div class="mr-3 flex h-5 w-5 items-center justify-center">
+						<div
+							class={[
+								'flex h-4 w-4 items-center justify-center rounded-full border-2',
+								selectedOption === option.arguments ? 'border-accent-400' : 'border-primary-400'
+							]}
+						>
+							{#if selectedOption === option.arguments}
+								<div class="bg-accent-400 h-2 w-2 rounded-full"></div>
+							{/if}
 						</div>
-						<div class="flex-1 text-left">
-							<div class="font-medium text-white">
-								{formatLaunchOptionName(option.type, gameName, option.description)}
-							</div>
+					</div>
+					<div class="flex text-left">
+						<div class="font-medium text-white">
+							{formatLaunchOptionName(option.type, gameName, option.description)}
 						</div>
 					</div>
 				</RadioGroup.Item>
@@ -73,7 +73,9 @@
 		You can disable this dialog by turning off "Show Steam launch options" in <a
 			href="/prefs"
 			onclick={() => (open = false)}
-			class="text-primary-400 hover:text-primary-300 underline">Settings</a
+			class="text-primary-400 hover:text-primary-300 underline"
+		>
+			Settings</a
 		>
 	</div>
 
